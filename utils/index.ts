4c78b24548f5a4a812f05f15d8a1930ff1bdac22
anuragrajanp <anuragrajanp@gmail.com>
2023-09-13 20:56:54 +0530

    show more button

--- utils/index.ts
+++ utils/index.ts
@@ -49,12 +49,14 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 };
 
 export const updateSearchParams = (type: string, value: string) => {
-	// get current URL search params
+	// Get the current URL search params
 	const searchParams = new URLSearchParams(window.location.search);
 
-	// set the specified search parameter to the given value
+	// Set the specified search parameter to the given value
 	searchParams.set(type, value);
 
-	const newPathname = `${window.location.search}?${searchParams.toString()}`;
+	// Set the specified search parameter to the given value
+	const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
 	return newPathname;
 };
