ec2402c7630c230eaf9ea7ebc3cd4d71aa1b20af
anuragrajanp <anuragrajanp@gmail.com>
2023-09-13 16:35:42 +0530

    searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,17 @@ import CustomFilter from "@/components/CustomFilter";
 import CarCard from "@/components/CarCard";
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
+import { HomeProps } from "@/types";
+import ShowMore from "@/components/ShowMore";
 
-export default async function Home() {
-	const allCars = await fetchCars();
+export default async function Home({ searchParams }: HomeProps) {
+	const allCars = await fetchCars({
+		manufacturer: searchParams.manufacturer || "",
+		year: searchParams.year || 2022,
+		fuel: searchParams.fuel || "",
+		limit: searchParams.limit || 10,
+		model: searchParams.model || "",
+	});
 
 	const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -30,11 +38,17 @@ export default async function Home() {
 				</div>
 
 				{!isDataEmpty ? (
-					<div className="home__cars-wrapper">
-						{allCars?.map((car) => (
-							<CarCard car={car} />
-						))}
-					</div>
+					<section>
+						<div className="home__cars-wrapper">
+							{allCars?.map((car) => (
+								<CarCard car={car} />
+							))}
+						</div>
+						<ShowMore
+							pageNumber={(searchParams.limit || 10) / 10}
+							isNext={(searchParams.limit || 10) > allCars.length}
+						/>
+					</section>
 				) : (
 					<div className="home__error-container">
 						<h2 className="font-bold text-xl text-black">NO RESULT</h2>
